4ee09197ad4f6d64aca48dded8b16f10b1cbcb68
suleka96 <sulekahelmini96comp@gmail.com>
2019-05-30 13:25:55 +0530

    Implement sample endpoint for feed forward nn

--- README.md
+++ README.md
@@ -3,22 +3,54 @@
 TensorMap is a web application that will allow the users to create machine learning algorithms visually. TensorMap supports reverse engineering of the visual layout to a Tensorflow implementation in preferred languages. The goal of the project is to let the beginners play with machine learning algorithms in Tensorflow without less background knowledge about the library.
 
 ## Getting Started
-Follow these steps to step TensorMap to your local machine...
-### Prerequisites
+Follow these steps to set up TensorMap on your local machine.
+
+First clone this repo by running
+```bash
+
+git clone https://github.com/scorelab/TensorMap.git
+```````````````````````````
+
+
+### Setting up Frontend
+
+#### Prerequisites
 * Node.js
 * Yarn
 * Npm
 
 ```bash
-git clone https://github.com/scorelab/TensorMap.git
 cd Tensormap
 yarn install
 npm start
 ```
+### Setting up Backend
+
+First, go into 'tensormap-server' folder
+
+```bash
+cd Tensormap
+cd tensormap-server
+```
+
+Then install all the required packages by running
+
+```bash
+pip install -r requirements.txt
+```
+
+To start the server run
+
+```bash
+python run.py
+```
+
+
 
 ## Built With
 
 * [Reactjs](https://reactjs.org/docs/getting-started.html) - The web framework used
+* [Flask](http://flask.pocoo.org/) - Backend 
 
 ## Contributing
 
---
+++ tensormap-server/app/resources/dlmodel.py
@@ -0,0 +1,52 @@
+from flask_restful import Resource
+from flask import request
+from flask import jsonify
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing.text import Tokenizer
+import tensorflow as tf
+import json
+from .. import socketio
+from flask_socketio import emit
+
+@socketio.on('nn_execute', namespace='/nn')
+def nn_execute(nnmodelconfig):
+
+    print(nnmodelconfig)
+
+    json_config = json.dumps(nnmodelconfig)
+
+    #sample model (IMDB) - Binary Classification
+    number_of_features = 1000
+
+    (train_data, train_target), (test_data, test_target) = imdb.load_data(num_words=number_of_features)
+
+    # Convert movie review data to one-hot encoded feature matrix
+    tokenizer = Tokenizer(num_words=number_of_features)
+    train_features = tokenizer.sequences_to_matrix(train_data, mode='binary')
+    test_features = tokenizer.sequences_to_matrix(test_data, mode='binary')  
+
+    #constructing model from json
+    model = keras.models.model_from_json(json_config)
+
+    model.compile(loss='binary_crossentropy', 
+                optimizer='rmsprop', 
+                metrics=['accuracy'])
+
+    model.fit(train_features,
+                      train_target, 
+                      epochs=3, 
+                      verbose=1, 
+                      batch_size=100) 
+
+    val_loss, val_acc = model.evaluate(test_features, test_target) 
+
+    results = {}
+    results['loss'] = float(val_loss)
+    results['accuracy']= float(val_acc)
+    results = json.dumps(results)
+        
+    #TO_DO - change according to frontend
+    emit('sample_response', results, namespace='/samplenamespace')
+
