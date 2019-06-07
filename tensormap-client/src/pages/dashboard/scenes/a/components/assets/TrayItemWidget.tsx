417165e90de2f00f8bcd5b233999a3c618ad2d18
orionpax00 <durgesh123.iitr@gmail.com>
2019-06-07 22:36:52 +0900

    Layer tray styled as material-ui accordians

--- tensormap-client/src/pages/dashboard/scenes/a/components/assets/TrayItemWidget.tsx
+++ tensormap-client/src/pages/dashboard/scenes/a/components/assets/TrayItemWidget.tsx
@@ -1,4 +1,10 @@
 import * as React from "react";
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 export interface TrayItemWidgetProps {
 	model: any;
@@ -17,15 +23,27 @@ export class TrayItemWidget extends React.Component<TrayItemWidgetProps, TrayIte
 	render() {
 		return (
 			<div
-				style={{ borderColor: this.props.color }}
-				draggable={true}
-				onDragStart={event => {
-					event.dataTransfer.setData("storm-diagram-node", JSON.stringify(this.props.model));
-				}}
-				className="tray-item"
-			>
-				{this.props.name}
-			</div>
+					style={{ borderColor: '#fff' }}
+					draggable={true}
+					onDragStart={event => {
+						event.dataTransfer.setData("storm-diagram-node", JSON.stringify(this.props.model));
+					}}
+					className="tray-item">
+      <ExpansionPanel square>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{this.props.name}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+						This is a {this.props.name}, drag it...
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
 		);
 	}
 }
