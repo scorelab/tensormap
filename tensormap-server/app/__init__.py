cb3ef2f1d1f95ac40d97bdaeee64c5a8e7178266
suleka96 <sulekahelmini96comp@gmail.com>
2019-06-23 14:03:26 +0530

    Add SQLALCHEMY configs

--- tensormap-server/app/__init__.py
+++ tensormap-server/app/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 
 socketio = SocketIO()
+db = SQLAlchemy()
 
 def create_app(debug=False):
     
     app = Flask(__name__)
     app.debug = debug
-    #test key
-    app.config['SECRET_KEY'] = 'secret'
+    app.config['SECRET_KEY'] = 'secret'#test key
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:pass@localhost/tensormap"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    from .resources import dl_blueprint
-    app.register_blueprint(dl_blueprint)
+    from .resources import main
+    app.register_blueprint(main)
 
     socketio.init_app(app)
+    db.init_app(app)
     return app
