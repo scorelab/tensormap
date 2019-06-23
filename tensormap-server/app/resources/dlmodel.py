74c86f72add5e454517e6741788825fb837cf5b9
suleka96 <sulekahelmini96comp@gmail.com>
2019-06-23 14:08:43 +0530

    Update dlmodel.py

--- tensormap-server/app/resources/dlmodel.py
+++ tensormap-server/app/resources/dlmodel.py
@@ -9,44 +9,62 @@ import tensorflow as tf
 import json
 from .. import socketio
 from flask_socketio import emit
+from ..common import validate_model_json, make_model_json
+
+
 
 @socketio.on('nn_execute', namespace='/nn')
 def nn_execute(nnmodelconfig):
 
-    print(nnmodelconfig)
+    resultString = validate_model_json.validate_json(nnmodelconfig)
 
-    json_config = json.dumps(nnmodelconfig)
+    if resultString == True:
 
-    #sample model (IMDB) - Binary Classification
-    number_of_features = 1000
+        modelJSON=make_model_json.makeKerasModel(nnmodelconfig)
 
-    (train_data, train_target), (test_data, test_target) = imdb.load_data(num_words=number_of_features)
+        np.random.seed(0)
 
-    # Convert movie review data to one-hot encoded feature matrix
-    tokenizer = Tokenizer(num_words=number_of_features)
-    train_features = tokenizer.sequences_to_matrix(train_data, mode='binary')
-    test_features = tokenizer.sequences_to_matrix(test_data, mode='binary')
+        number_of_features = 1000
 
-    #constructing model from json
-    model = keras.models.model_from_json(json_config)
+        np_load_old = np.load
 
-    model.compile(loss='binary_crossentropy',
-                optimizer='rmsprop',
-                metrics=['accuracy'])
+        np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
 
-    model.fit(train_features,
-                      train_target,
-                      epochs=3,
-                      verbose=1,
-                      batch_size=100)
+        (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=number_of_features)
 
-    val_loss, val_acc = model.evaluate(test_features, test_target)
+        np.load = np_load_old
 
-    results = {}
-    results['loss'] = float(val_loss)
-    results['accuracy']= float(val_acc)
-    results = json.dumps(results)
+        tokenizer = Tokenizer(num_words=number_of_features)
+        train_features = tokenizer.sequences_to_matrix(train_data, mode='binary')
+        test_features = tokenizer.sequences_to_matrix(test_data, mode='binary')
 
-    #TO_DO - change according to frontend
-    emit('sample_response', results, namespace='/samplenamespace')
-    # emit("valla...")
+        model = keras.models.model_from_json(modelJSON)
+
+        model.compile(loss='binary_crossentropy', 
+                        optimizer='rmsprop', 
+                        metrics=['accuracy'])
+
+        model.fit(train_features,
+                        train_labels, 
+                        epochs=3, 
+                        verbose=1, 
+                        batch_size=100) 
+
+        val_loss, val_acc = model.evaluate(test_features, test_labels) 
+
+        results = {}
+        results['loss'] = float(val_loss)
+        results['accuracy']= float(val_acc)
+        results = json.dumps(results)        
+            
+        #TO_DO - change according to frontend
+        emit('sample_response', results, namespace='/samplenamespace')
+    
+    else:
+        results = {}
+        results['error'] = resultString
+        results = json.dumps(results)
+
+        #TO_DO - change according to frontend
+        emit('sample_response', results, namespace='/samplenamespace')
+
