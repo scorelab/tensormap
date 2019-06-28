324ad1850c661669e0e3bb0660d1b83d444b569a
orionpax00 <durgesh123.iitr@gmail.com>
2019-06-29 00:05:01 +0900

    New location and color functions

---
+++ tensormap-client/src/pages/dashboard/scenes/a/components/assets/GroupTheme.js
@@ -0,0 +1,13 @@
+
+//generate new color functions
+
+function getNewColor(inter) {
+  return //new color
+}
+
+
+//generate new location for grouped nodes
+
+function getNewLocation(inter) {
+  return //new location
+}
