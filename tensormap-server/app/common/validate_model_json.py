4223c1f3384eb7ad277935176a24c69290fc77e2
suleka96 <sulekahelmini96comp@gmail.com>
2019-07-13 15:10:56 +0530

    Update validation code

--- tensormap-server/app/common/validate_model_json.py
+++ tensormap-server/app/common/validate_model_json.py
@@ -1,63 +1,119 @@
 
-errorString = " "
+
 
 def validate_json(content):
 
-    global errorString
+    errorString = " "
 
     layers = {'input': False, 'hidden':False, 'output':False}
    
     inputCounter = 0
-    hiddenCounter = 0
     outputCounter = 0
-    node_param_validation = True
+    experiment_validation = True
 
-    for i in range(len(content["graph"][0]["nodes"])):
-        if "Input" in content["graph"][0]["nodes"][i]["name"]:
+    for i in range(len(content["graph"][0]["layers"])):
+        if "Input" in content["graph"][0]["layers"][i]["name"]:
             layers['input']= True
             inputCounter += 1
-        elif "Hidden" in content["graph"][0]["nodes"][i]["name"]:
+        elif "Hidden" in content["graph"][0]["layers"][i]["name"]:
             layers['hidden']= True
-            hiddenCounter += 1
-        elif "Output" in content["graph"][0]["nodes"][i]["name"]:
+        elif "Output" in content["graph"][0]["layers"][i]["name"]:
             layers['output']= True
             outputCounter += 1
 
+
     if layers['input'] == False and layers['output'] and layers['hidden']:
         errorString = "Input Layer is Missing."
+        experiment_validation = False
+    elif layers['input'] == False and layers['output'] and layers['hidden'] == False:
+        errorString = "Input Layer and Hidden Layer are Missing."
+        experiment_validation = False
+    elif layers['input'] and layers['output'] == False and layers['hidden'] == False:
+        errorString = "Output Layer and Hidden Layer are Missing."
+        experiment_validation = False
     elif layers['output'] == False and layers['input'] and layers['hidden'] :
         errorString = "Output Layer is Missing."
+        experiment_validation = False
     elif layers['input'] == False and layers['output'] == False and layers['hidden']:
-        errorString = "Output Layer and Input Layer is Missing."
+        errorString = "Output Layer and Input Layer are Missing."
+        experiment_validation = False
     elif layers['input'] == False and layers['output'] == False and layers['hidden'] == False:
         errorString = "No Layers Defined."
+        experiment_validation = False
     elif layers['hidden'] == False and layers['output'] and layers['input']:
         errorString = "No Hidden Layers Defined."
+        experiment_validation = False
     
     elif inputCounter != 1:
         errorString = "Too Many Input Layers Defined."
+        experiment_validation = False
     elif outputCounter != 1:
         errorString = "Too Many Output Layers Defined."
+        experiment_validation = False
 
-    totalNumLayers = len(content["graph"][0]["nodes"])
+    totalNumLayers = len(content["graph"][0]["layers"])
+
+    for i in range(len(content["graph"][0]["layers"])):
+        if "Output" in content["graph"][0]["layers"][i]["name"]:
+            Outputid = content["graph"][0]["layers"][i]["id"]
+            break
    
-    if len(content["node_param"]) != totalNumLayers:
-        errorString += " Too many or Less Layer Parameters in node_param."
-        node_param_validation = False
+    if len(content["layer_param"]) != totalNumLayers:
+        errorString += " Too many or Less Layer Parameters in layer_param."
+        experiment_validation = False
     else:     
-        for i in range(len(content["node_param"])):
+        for i in range(len(content["layer_param"])):
             try:
-                units = float(content["node_param"][i]["param"][0]["units"])
+                units = float(content["layer_param"][i]["param"][0]["units"])
             except ValueError:
                 errorString += " units is a digit."
-                node_param_validation = False
+                experiment_validation = False
                 break
-            if not (content["node_param"][i]["param"][0]["activation"] == "relu" or content["node_param"][i]["param"][0]["activation"] =="sigmoid" or content["node_param"][i]["param"][0]["activation"]=="tanh"):
+            if not (content["layer_param"][i]["param"][0]["activation"] == "relu" or content["layer_param"][i]["param"][0]["activation"] =="sigmoid" or content["layer_param"][i]["param"][0]["activation"]=="tanh" or content["layer_param"][i]["param"][0]["activation"]=="softmax"):
                 errorString += " Invalid Activation."
-                node_param_validation = False
+                experiment_validation = False
                 break
-      
-    if layers['input'] and layers['output'] and layers['hidden'] and inputCounter == 1 and outputCounter == 1 and node_param_validation:
+            if content["layer_param"][i]["param"][0]["units"] == 0:
+                errorString += " Number of units cannot be zero."
+                experiment_validation = False
+                break
+            if content["layer_param"][i]["id"] == Outputid:
+                if content["experiment_info"]["type"] == "classification":
+                    if content["layer_param"][i]["param"][0]["activation"] == "relu" or content["layer_param"][i]["param"][0]["activation"]=="tanh":
+                        errorString += " Invalid output Activation for classification."
+                        experiment_validation = False
+                        break
+                    elif content["experiment_info"]["multiclass"] == "True" and content["layer_param"][i]["param"][0]["activation"]=="sigmoid":
+                        errorString += " Invalid output Activation for multi-class classification."
+                        experiment_validation = False
+                    elif content["experiment_info"]["multiclass"] == "Flase" and content["layer_param"][i]["param"][0]["activation"]=="softmax":
+                        errorString += " Invalid output Activation for binary classification."
+                        experiment_validation = False
+                    elif content["experiment_info"]["multiclass"] == "Flase" and content["layer_param"][i]["param"][0]["units"] > 1:
+                        errorString += " Invalid number of output units for binary classification."
+                        experiment_validation = False
+                    elif content["experiment_info"]["multiclass"] == "True" and content["layer_param"][i]["param"][0]["units"] == 1:
+                        errorString += " Invalid number of output units for multi-class classification."
+                        experiment_validation = False                 
+                elif content["experiment_info"]["type"] == "regression":
+                    if content["layer_param"][i]["param"][0]["activation"] == "sigmoid" or content["layer_param"][i]["param"][0]["activation"]=="softmax":
+                        errorString += " Invalid output Activation for regression."
+                        experiment_validation = False
+                        break
+                    elif content["layer_param"][i]["param"][0]["units"] > 1:
+                        errorString += " Invalid number of output units for regression."
+                        experiment_validation = False
+                        break
+
+    if content["experiment_info"]["batch_size"] >= content["experiment_info"]["dataset_length"]:
+        errorString += " Batch size should be less than dataset length."
+        experiment_validation = False       
+
+    if content["graph"][0]["num_unassigned_nodes"]  != 0:
+        errorString += " Unassigned nodes in workspace."
+        experiment_validation = False
+ 
+    if experiment_validation:
         errorString = True    
 
     print(errorString)
