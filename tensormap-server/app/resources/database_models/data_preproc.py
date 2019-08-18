8a552ffa8cb621ffb9410c74c2d92b81097fe205
suleka96 <sulekahelmini96comp@gmail.com>
2019-08-19 04:00:30 +0530

    Add backend route to save features, labels and test percentage

--- tensormap-server/app/resources/database_models/data_preproc.py
+++ tensormap-server/app/resources/database_models/data_preproc.py
@@ -5,15 +5,24 @@ class dataset(db.Model):
     fileName = db.Column(VARCHAR(50), primary_key=True)
     filePath = db.Column(VARCHAR(1000))
     fileFormat = db.Column(VARCHAR(50))
+    features = db.Column(VARCHAR(1000))
+    labels = db.Column(VARCHAR(1000))
+    testPercentage = db.Column(INTEGER)
 
-    def __init__(self, fileName, filePath, fileFormat):
+    def __init__(self, fileName, filePath, fileFormat,features,labels,testPercentage):
         self.fileName = fileName
         self.filePath = filePath
         self.fileFormat = fileFormat
+        self.features = features
+        self.labels = labels
+        self.testPercentage = testPercentage
 
     def serialize(self):
         return  {
             'fileName': self.fileName,
             'filePath': self.filePath,
-            'fileFormat': self.fileFormat
+            'fileFormat': self.fileFormat,
+            'features' : self.features,
+            'labels' :self.labels,
+            'testPercentage' : self.testPercentage
         }
