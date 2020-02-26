bb8da65523d8075e3cd2112ecdb8bf186d00a3e0
srivastava9 <krishu4626@gmail.com>
2020-02-27 02:04:32 +0530

    bug-fixes in viewdata and table

--- tensormap-client/src/pages/viewdata/ViewData.jsx
+++ tensormap-client/src/pages/viewdata/ViewData.jsx
@@ -1,35 +1,36 @@
-import {withStyles} from '@material-ui/core'
-import PropTypes    from 'prop-types'
-import * as React   from 'react'
-import styles       from './ViewData.styles'
-import Table from './assets/Table'
+import { withStyles } from "@material-ui/core";
+import PropTypes from "prop-types";
+import * as React from "react";
+import styles from "./ViewData.styles";
+import Table from "./assets/Table";
 
 class ViewData extends React.Component {
   constructor() {
     super();
-    var Httpreq = new XMLHttpRequest();
-    Httpreq.open("GET", 'http://localhost:5000/viewData', false);
-    Httpreq.send(null);
     this.state = {
-      data:JSON.parse(Httpreq.responseText),
+      data: {}
     };
   }
-
-
+  componentDidMount() {
+    fetch(`${baseURL}viewData`)
+      .then(response => response.json())
+      .then(data => this.setState({ data }))
+      .catch(error => {
+        console.log(error);
+        alert("There was some error pulling data");
+      });
+  }
   render() {
     console.log(this.state.data);
-    const {classes} = this.props
+    const { classes } = this.props;
 
-    return (
-        <Table data={this.state.data}/>
-    )
+    return <Table data={this.state.data} />;
   }
-
 }
 
 ViewData.propTypes = {
   classes: PropTypes.object.isRequired,
-  theme  : PropTypes.object.isRequired,
-}
+  theme: PropTypes.object.isRequired
+};
 
-export default withStyles(styles, {withTheme: true})(ViewData)
+export default withStyles(styles, { withTheme: true })(ViewData);
--- tensormap-client/src/pages/viewdata/assets/Table.jsx
+++ tensormap-client/src/pages/viewdata/assets/Table.jsx
@@ -1,47 +1,45 @@
-import React from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import DeleteIcon from '@material-ui/icons/Delete';
-import OpenInNew from '@material-ui/icons/OpenInNew';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import { lighten } from '@material-ui/core/styles/colorManipulator';
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import DeleteIcon from "@material-ui/icons/Delete";
+import OpenInNew from "@material-ui/icons/OpenInNew";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import { lighten } from "@material-ui/core/styles/colorManipulator";
 
 let counter = 0;
 function createData(id, name, dtype) {
   counter += 1;
-  return { id: id, name, dtype};
+  return { id: id, name, dtype };
 }
 
 function getDeleteHandler(idx) {
-    var Httpreq = new XMLHttpRequest();
-    Httpreq.open("GET", 'http://localhost:5000/deleteData?id=' + idx, true);
-    Httpreq.send(null);
-    console.log("completed")
+  var Httpreq = new XMLHttpRequest();
+  Httpreq.open("GET", "http://localhost:5000/deleteData?id=" + idx, true);
+  Httpreq.send(null);
+  console.log("completed");
 }
 
-
 const rows = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Dataset Name' },
-  { id: 'dtype', numeric: true, disablePadding: false, label: 'Data Type' },
-  { id: 'icons', numeric: true, disablePadding: false, label: 'operations' },
+  { id: "name", numeric: false, disablePadding: true, label: "Dataset Name" },
+  { id: "dtype", numeric: true, disablePadding: false, label: "Data Type" },
+  { id: "icons", numeric: true, disablePadding: false, label: "operations" }
 ];
-
 
 class EnhancedTableHead extends React.Component {
   createSortHandler = property => event => {
@@ -49,7 +47,13 @@ class EnhancedTableHead extends React.Component {
   };
 
   render() {
-    const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
+    const {
+      onSelectAllClick,
+      order,
+      orderBy,
+      numSelected,
+      rowCount
+    } = this.props;
 
     return (
       <TableHead>
@@ -65,13 +69,13 @@ class EnhancedTableHead extends React.Component {
             row => (
               <TableCell
                 key={row.id}
-                align={row.numeric ? 'right' : 'left'}
-                padding={row.disablePadding ? 'none' : 'default'}
+                align={row.numeric ? "right" : "left"}
+                padding={row.disablePadding ? "none" : "default"}
                 sortDirection={orderBy === row.id ? order : false}
               >
                 <Tooltip
                   title="Sort"
-                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  placement={row.numeric ? "bottom-end" : "bottom-start"}
                   enterDelay={300}
                 >
                   <TableSortLabel
@@ -84,7 +88,7 @@ class EnhancedTableHead extends React.Component {
                 </Tooltip>
               </TableCell>
             ),
-            this,
+            this
           )}
         </TableRow>
       </TableHead>
@@ -98,32 +102,32 @@ EnhancedTableHead.propTypes = {
   onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.string.isRequired,
   orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
+  rowCount: PropTypes.number.isRequired
 };
 
 const toolbarStyles = theme => ({
   root: {
-    paddingRight: theme.spacing.unit,
+    paddingRight: theme.spacing.unit
   },
   highlight:
-    theme.palette.type === 'light'
+    theme.palette.type === "light"
       ? {
           color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85)
         }
       : {
           color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
+          backgroundColor: theme.palette.secondary.dark
         },
   spacer: {
-    flex: '1 1 100%',
+    flex: "1 1 100%"
   },
   actions: {
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.secondary
   },
   title: {
-    flex: '0 0 auto',
-  },
+    flex: "0 0 auto"
+  }
 });
 
 let EnhancedTableToolbar = props => {
@@ -132,7 +136,7 @@ let EnhancedTableToolbar = props => {
   return (
     <Toolbar
       className={classNames(classes.root, {
-        [classes.highlight]: numSelected > 0,
+        [classes.highlight]: numSelected > 0
       })}
     >
       <div className={classes.title}>
@@ -150,7 +154,10 @@ let EnhancedTableToolbar = props => {
       <div className={classes.actions}>
         {numSelected > 0 ? (
           <Tooltip title="Delete">
-            <IconButton aria-label="Delete" onClick={event => handledelete(event, selected)} >
+            <IconButton
+              aria-label="Delete"
+              onClick={event => handledelete(event, selected)}
+            >
               <DeleteIcon />
             </IconButton>
           </Tooltip>
@@ -169,75 +176,81 @@ let EnhancedTableToolbar = props => {
 EnhancedTableToolbar.propTypes = {
   classes: PropTypes.object.isRequired,
   numSelected: PropTypes.number.isRequired,
-  handledelete: PropTypes.func.isRequired,
+  handledelete: PropTypes.func.isRequired
 };
 
 EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
 
 const styles = theme => ({
   root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    width: "100%",
+    marginTop: theme.spacing.unit * 3
   },
   table: {
-    minWidth: 540,
+    minWidth: 540
   },
   tableWrapper: {
-    overflowX: 'auto',
-  },
+    overflowX: "auto"
+  }
 });
 
 class EnhancedTable extends React.Component {
-  state = {
-    selected: [],
-    data_:[],
-    data: [
-    ],
-    page: 0,
-    rowsPerPage: 5,
-  };
-
-  componentDidMount(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: [],
+      data_: [],
+      data: [],
+      page: 0,
+      rowsPerPage: 5
+    };
     this.setState({
-      data_:this.props.data
+      data_: this.props.data
     });
-
-    var data__ = [];
-    this.props.data.forEach((x) => {
-      data__.push(createData(x.id,x.name,x.fileFormat))
-    })
-
-    this.setState({
-      data :data__
-    })
   }
 
+  componentWillReceiveProps(nextProps) {
+    var data__ = [];
+
+    // this.props.data.forEach(x => {
+
+    //   data__.push(createData(x.id, x.fileName, x.fileFormat));
+    // });
+
+    const data_array = Object.values(nextProps.data);
+    for (let i = 0; i < data_array.length; i++) {
+      data__.push(
+        createData(i, data_array[i].fileName, data_array[i].fileFormat)
+      );
+    }
+    this.setState({
+      data: data__
+    });
+  }
   handleDelete = (event, id) => {
     var tmp_data = [];
-    id.forEach((x) =>{
-        getDeleteHandler(x);
-        this.state.data.forEach((y) => {
-          if(y.id == x){
-            console.log(x, "deleted")
-          }
-          else{
-            tmp_data.push(y)
-          }
-        })
-      }
-    );
+    id.forEach(x => {
+      getDeleteHandler(x);
+      this.state.data.forEach(y => {
+        if (y.id == x) {
+          console.log(x, "deleted");
+        } else {
+          tmp_data.push(y);
+        }
+      });
+    });
 
     this.setState({
-      data:tmp_data
-    })
-  }
+      data: tmp_data
+    });
+  };
 
   handleRequestSort = (event, property) => {
     const orderBy = property;
-    let order = 'desc';
+    let order = "desc";
 
-    if (this.state.orderBy === property && this.state.order === 'desc') {
-      order = 'asc';
+    if (this.state.orderBy === property && this.state.order === "desc") {
+      order = "asc";
     }
 
     this.setState({ order, orderBy });
@@ -265,7 +278,7 @@ class EnhancedTable extends React.Component {
     } else if (selectedIndex > 0) {
       newSelected = newSelected.concat(
         selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
+        selected.slice(selectedIndex + 1)
       );
     }
 
@@ -282,22 +295,22 @@ class EnhancedTable extends React.Component {
 
   isSelected = id => this.state.selected.indexOf(id) !== -1;
 
-
   render() {
     const { classes } = this.props;
     const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
-
-
+    const emptyRows =
+      rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
 
     return (
       <Grid container spacing={24}>
-        <Grid item xs={2}>
-        </Grid>
+        <Grid item xs={2}></Grid>
         <Grid item xs={8}>
-
           <Paper className={classes.root}>
-            <EnhancedTableToolbar numSelected={selected.length} handledelete={this.handleDelete} selected = {selected}/>
+            <EnhancedTableToolbar
+              numSelected={selected.length}
+              handledelete={this.handleDelete}
+              selected={selected}
+            />
             <div className={classes.tableWrapper}>
               <Table className={classes.table} aria-labelledby="tableTitle">
                 <EnhancedTableHead
@@ -323,14 +336,22 @@ class EnhancedTable extends React.Component {
                           selected={isSelected}
                         >
                           <TableCell padding="checkbox">
-                            <Checkbox checked={isSelected} onClick={event => this.handleClick(event, n.id)}/>
+                            <Checkbox
+                              checked={isSelected}
+                              onClick={event => this.handleClick(event, n.id)}
+                            />
                           </TableCell>
                           <TableCell component="th" scope="row" padding="none">
                             {n.name}
                           </TableCell>
                           <TableCell align="right">{n.dtype}</TableCell>
                           <TableCell align="right">
-                            <Link to={"/visualize/" + n.id} >
+                            <Link
+                              to={{
+                                pathname: `/visualize/${n.name}`,
+                                state: { fileName: n.name, dtype: n.dtype }
+                              }}
+                            >
                               <IconButton aria-label="View">
                                 <OpenInNew />
                               </IconButton>
@@ -354,25 +375,24 @@ class EnhancedTable extends React.Component {
               rowsPerPage={rowsPerPage}
               page={page}
               backIconButtonProps={{
-                'aria-label': 'Previous Page',
+                "aria-label": "Previous Page"
               }}
               nextIconButtonProps={{
-                'aria-label': 'Next Page',
+                "aria-label": "Next Page"
               }}
               onChangePage={this.handleChangePage}
               onChangeRowsPerPage={this.handleChangeRowsPerPage}
             />
           </Paper>
         </Grid>
-        <Grid item xs={2}>
-        </Grid>
+        <Grid item xs={2}></Grid>
       </Grid>
     );
   }
 }
 
 EnhancedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(EnhancedTable);
