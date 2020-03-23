3589a17c893e2f0cef164b13be659e251b7433b0
piyush-palta <43702687+piyush-palta@users.noreply.github.com>
2020-03-24 01:43:15 +0530

    Create index.js

---
+++ tensormap-client/src/pages/login/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Form from './login';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Form/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
