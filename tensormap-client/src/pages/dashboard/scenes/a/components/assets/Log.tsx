c72647b999c34c150ddf010874c538720b0bb112
srivastava9 <krishu4626@gmail.com>
2020-03-26 16:13:59 +0530

    made functionality for exporting code to python using file-saver library

--- tensormap-client/src/pages/dashboard/scenes/a/components/assets/Log.tsx
+++ tensormap-client/src/pages/dashboard/scenes/a/components/assets/Log.tsx
@@ -5,7 +5,7 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import { Button } from "@material-ui/core";
-
+import Filesaver from "file-saver";
 import GetAppIcon from "@material-ui/icons/GetApp";
 interface TabContainerProps {
   children?: React.ReactNode;
@@ -40,6 +40,10 @@ export default function SimpleTabs(props: SimpleTabsProps) {
   let newText = props.code.split("\n").map((item, i) => {
     return <p key={i}>{item}</p>;
   });
+  function exportPython() {
+    var blob = new Blob([props.code], { type: "text/x-python;charset=utf-8" });
+    Filesaver.saveAs(blob, "hello-world.py");
+  }
 
   return (
     <div className="log_main">
@@ -54,7 +58,11 @@ export default function SimpleTabs(props: SimpleTabsProps) {
         {value === 1 && (
           <div className={props.divStyle}>
             <TabContainer>{newText}</TabContainer>
-            <Button variant="contained" className={props.exportStyle}>
+            <Button
+              variant="contained"
+              className={props.exportStyle}
+              onClick={exportPython}
+            >
               <GetAppIcon fontSize="small" /> Export to Python
             </Button>
           </div>
