faaad0b295af7bbfc0676f9cb75572ead8fef0b7
srivastava9 <krishu4626@gmail.com>
2020-03-31 20:10:52 +0530

    made home page for tensormap

--- tensormap-client/src/pages/dashboard/components/header/Header.jsx
+++ tensormap-client/src/pages/dashboard/components/header/Header.jsx
@@ -1,48 +1,47 @@
-import {withStyles} from '@material-ui/core'
-import AppBar       from '@material-ui/core/AppBar/index'
-import IconButton   from '@material-ui/core/IconButton/index'
-import Toolbar      from '@material-ui/core/Toolbar/index'
-import Typography   from '@material-ui/core/Typography/index'
-import MenuIcon     from '@material-ui/icons/Menu'
-import PropTypes    from 'prop-types'
-import * as React   from 'react'
-import styles       from './Header.styles'
+import { withStyles } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar/index";
+import IconButton from "@material-ui/core/IconButton/index";
+import Toolbar from "@material-ui/core/Toolbar/index";
+import Typography from "@material-ui/core/Typography/index";
+import MenuIcon from "@material-ui/icons/Menu";
+import PropTypes from "prop-types";
+import * as React from "react";
+import styles from "./Header.styles";
 
 class Header extends React.Component {
   state = {
-    mobileOpen: false,
-  }
+    mobileOpen: false
+  };
 
   handleDrawerToggle = () => {
-    this.setState(state => ({mobileOpen: !state.mobileOpen}))
-  }
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  };
 
   render() {
-    const {classes} = this.props
+    const { classes } = this.props;
 
     return (
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-                color="inherit"
-                aria-label="Open drawer"
-                onClick={this.handleDrawerToggle}
-                className={classes.menuButton}
-            >
-              <MenuIcon/>
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              [Title]
-            </Typography>
-          </Toolbar>
-        </AppBar>
-    )
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            onClick={this.handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" noWrap>
+            TensorMap
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
   }
-
 }
 
 Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-}
+  classes: PropTypes.object.isRequired
+};
 
-export default withStyles(styles, {withTheme: true})(Header)
+export default withStyles(styles, { withTheme: true })(Header);
