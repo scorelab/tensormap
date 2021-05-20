24aa3b8dbec6a14d4858ea10160e373c5820c4c0
Asela Dasanayaka <aselarbd@gmail.com>
2021-05-20 20:21:42 +0200

    [TensorMap-revamp] File upload mechanism created
    File upload mechanism through flask rest api is created. only CSV files allowed. Empty check as well.

--- tensormap-server-revamp/endpoints/DataUpload/models.py
+++ tensormap-server-revamp/endpoints/DataUpload/models.py
@@ -1,12 +1,10 @@
 from shared.utils import get_db_ref
 
+
 db = get_db_ref()
 
 
 class DataFile(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    file_name = db.Column(db.String(100), nullable=False)
-    file_type = db.Column(db.String(10), nullable=False)
-    target = db.relationship("DataProcess", uselist=False, cascade="all,delete", backref="file")
-    created_on = db.Column(db.DateTime, server_default=db.func.now())
-    updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    file_name = db.Column(db.String(100))
+    file_type = db.Column(db.String(10))
--- tensormap-server-revamp/setup/settings.py
+++ tensormap-server-revamp/setup/settings.py
@@ -1,14 +1,8 @@
 from shared.utils import get_db_ref, create_db_connection
 from shared.services.config import get_configs
-from flask_migrate import Migrate
 import os
 
 configs = get_configs()
-
-
-"""
-All the application related settings handle through here
-"""
 
 
 class SettingUp:
@@ -16,16 +10,13 @@ class SettingUp:
     def __init__(self, app):
         self.app = app
         self.app.config['SECRET_KEY'] = os.getenv("secret_key")
-        self.file_setup()
+        app.config['UPLOAD_FOLDER'] = configs['api']['upload']['folder']
+        app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
         self.database_setup()
         self.swagger_setup()
 
     def swagger_setup(self):
         pass
-
-    def file_setup(self):
-        self.app.config['UPLOAD_FOLDER'] = configs['api']['upload']['folder']
-        self.app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
 
     def database_setup(self):
         connection = create_db_connection()
@@ -34,5 +25,5 @@ class SettingUp:
         self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         db = get_db_ref()
         db.init_app(self.app)
-        migrate = Migrate(self.app, db)
-
+        with self.app.app_context():
+            db.create_all()
