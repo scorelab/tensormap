4a4c778c6ca62dc3cdf56009ffed9729acf45c79
Asela Dasanayaka <aselarbd@gmail.com>
2021-05-20 20:21:42 +0200

    [TensorMap-revamp] File upload mechanism created
    File upload mechanism through flask rest api is created. only CSV files allowed. Empty check as well.

--- tensormap-server-revamp/endpoints/DataUpload/models.py
+++ tensormap-server-revamp/endpoints/DataUpload/models.py
@@ -1,13 +1,10 @@
 from shared.utils import get_db_ref
 
+
 db = get_db_ref()
 
 
 class DataFile(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    file_name = db.Column(db.String(100), nullable=False)
-    file_type = db.Column(db.String(10), nullable=False)
-    target = db.relationship("endpoints.DataProcess.models.DataProcess", uselist=False, cascade="all,delete", backref="file")
-    associated_model = db.relationship("endpoints.DeepLearning.models.ModelBasic", backref="file")
-    created_on = db.Column(db.DateTime, server_default=db.func.now())
-    updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    file_name = db.Column(db.String(100))
+    file_type = db.Column(db.String(10))
