47076ae2ce2843248915f5638be75f2dae5d31f0
Asela Dasanayaka <aselarbd@gmail.com>
2021-05-21 20:17:08 +0200

    [TensorMap-revamp] Added get all uploaded file API and delete file by ID API
    Added two new API endpoints to DataUpload API

--- tensormap-server-revamp/endpoints/DataUpload/views.py
+++ tensormap-server-revamp/endpoints/DataUpload/views.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from endpoints.DataUpload.validators import data_upload_post_validator
-from endpoints.DataUpload.services import add_file_service
+from endpoints.DataUpload.services import add_file_service, get_all_files_service, delete_one_file_by_id_service
 
 
 class UploadDataFile(Resource):
@@ -9,3 +9,12 @@ class UploadDataFile(Resource):
         validator = data_upload_post_validator()
         args = validator.parse_args()
         return add_file_service()
+
+    def get(self):
+        return get_all_files_service()
+
+
+class UploadFileIDOperations(Resource):
+
+    def delete(self, file_id):
+        return delete_one_file_by_id_service(file_id=file_id)
