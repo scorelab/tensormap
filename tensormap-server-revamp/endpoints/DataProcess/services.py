ea8627d4921cc9a5c2f64587b989df20cb1961f9
Asela Dasanayaka <aselarbd@gmail.com>
2021-05-23 22:13:27 +0200

    [TensorMap-revamp] added one-to-one relationship between DataUpload and DataProcess models
    Modified models to have a 1-1 relationship and cascade delete option added

--- tensormap-server-revamp/endpoints/DataProcess/services.py
+++ tensormap-server-revamp/endpoints/DataProcess/services.py
@@ -9,7 +9,7 @@ def add_target_service(incoming):
     target = incoming["target"]
 
     if DataFile.query.filter_by(id=file_id).count() > 0:
-        data_process = DataProcess(file_id=file_id, target=target)
+        data_process = DataProcess(file_id=file_id, file=DataFile.query.filter_by(id=file_id).first(), target=target)
         save_one_record(record=data_process)
         return generic_response(status_code=201, success=True, message="Target field added successfully")
     else:
@@ -20,10 +20,8 @@ def get_all_targets_service():
     process_files = DataProcess.query.all()
     data = []
     for file in process_files:
-        if DataFile.query.filter_by(id=file.file_id).count() > 0:
-            file_data = DataFile.query.filter_by(id=file.file_id).first()
-            data.append({"file_id": file.file_id, "file_name": file_data.file_name, "file_type": file_data.file_type,
-                         "target_field": file.target})
+        data.append({"file_id": file.file_id, "file_name": file.file.file_name, "file_type": file.file.file_type,
+                     "target_field": file.target})
     return generic_response(status_code=200, success=True, message="Target fields of all files received successfully",
                             data=data)
 
@@ -42,10 +40,10 @@ def delete_one_target_by_id_service(file_id):
 
 def get_one_target_by_id_service(file_id):
     if DataFile.query.filter_by(id=file_id).count() > 0:
-        file = DataFile.query.filter_by(id=file_id).first()
         if DataProcess.query.filter_by(file_id=file_id).count() > 0:
             target_record = DataProcess.query.filter_by(file_id=file_id).first()
-            data = {"file_name": file.file_name, "file_type": file.file_type, "target_field": target_record.target}
+            data = {"file_name": target_record.file.file_name, "file_type": target_record.file.file_type,
+                    "target_field": target_record.target}
             return generic_response(status_code=200, success=True,
                                     message="Target fields of all files received successfully",
                                     data=data)
