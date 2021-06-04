6d1dc6127e7e047019c5446a6ed9a1f8cb0aad58
Asela Dasanayaka <aselarbd@gmail.com>
2021-06-04 13:44:55 +0200

    [TensorMap-revamp] DL model skeleton created
    Created DL model skeleton so functions can be grow around it

--- tensormap-server-revamp/setup/urls.py
+++ tensormap-server-revamp/setup/urls.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
 from endpoints.DataUpload.urls import data_urls
 from endpoints.DataProcess.urls import process_urls
+from endpoints.DeepLearning.urls import model_urls
 
 """
 To add a new urls to the application, all the url functions defined in the relevant files, need to be register here.
@@ -15,3 +16,4 @@ class MainURLRegister:
         MainURLRegister.api_ref = api
         data_urls(api=api)
         process_urls(api=api)
+        model_urls(api=api)
