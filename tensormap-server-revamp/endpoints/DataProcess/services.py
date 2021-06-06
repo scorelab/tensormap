ab63de46d359899a58d20163965bdd276f9ef347
Asela Dasanayaka <aselarbd@gmail.com>
2021-06-06 15:56:01 +0200

    [TensorMap-revamp] DL model code generation
    Created DL code generation using params

--- tensormap-server-revamp/endpoints/DataProcess/services.py
+++ tensormap-server-revamp/endpoints/DataProcess/services.py
@@ -2,11 +2,12 @@ from endpoints.DataProcess.models import DataProcess
 from endpoints.DataUpload.models import DataFile
 from shared.utils import save_one_record, delete_one_record
 from shared.request.response import generic_response
+from shared.constants import *
 
 
 def add_target_service(incoming):
-    file_id = incoming["file_id"]
-    target = incoming["target"]
+    file_id = incoming[FILE_ID]
+    target = incoming[FILE_TARGET_FIELD]
 
     if DataFile.query.filter_by(id=file_id).count() > 0:
         data_process = DataProcess(file_id=file_id, file=DataFile.query.filter_by(id=file_id).first(), target=target)
@@ -20,8 +21,8 @@ def get_all_targets_service():
     process_files = DataProcess.query.all()
     data = []
     for file in process_files:
-        data.append({"file_id": file.file_id, "file_name": file.file.file_name, "file_type": file.file.file_type,
-                     "target_field": file.target})
+        data.append({FILE_ID: file.file_id, FILE_NAME: file.file.file_name, FILE_TYPE: file.file.file_type,
+                     FILE_TARGET: file.target})
     return generic_response(status_code=200, success=True, message="Target fields of all files received successfully",
                             data=data)
 
@@ -42,8 +43,8 @@ def get_one_target_by_id_service(file_id):
     if DataFile.query.filter_by(id=file_id).count() > 0:
         if DataProcess.query.filter_by(file_id=file_id).count() > 0:
             target_record = DataProcess.query.filter_by(file_id=file_id).first()
-            data = {"file_name": target_record.file.file_name, "file_type": target_record.file.file_type,
-                    "target_field": target_record.target}
+            data = {FILE_NAME: target_record.file.file_name, FILE_TYPE: target_record.file.file_type,
+                    FILE_TARGET: target_record.target}
             return generic_response(status_code=200, success=True,
                                     message="Target fields of all files received successfully",
                                     data=data)
---
+++ tensormap-server-revamp/templates/code-templates/multi-class-all-float-classification-csv.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import yaml
+import json
+
+
+def deep_learning_model():
+    features = pd.read_csv("{{data.dataset.file_name}}")
+
+    # Split Training and testing sets
+    training_features = features.sample(frac={{data.dataset.training_split}}, random_state=200)
+    training_labels = training_features.pop('{{data.dataset.target_field}}')
+
+    testing_features = features.drop(training_features.index)
+    testing_labels = testing_features.pop('{{data.dataset.target_field}}')
+
+    x_training, y_training = (training_features.to_numpy(), training_labels.to_numpy())
+    x_testing, y_testing = (testing_features.to_numpy(), testing_labels.to_numpy())
+
+    # Preprocessing - scaling
+    x_training = x_training / np.linalg.norm(x_training)
+    x_testing = x_testing / np.linalg.norm(x_testing)
+
+    json_string = json.dumps(yaml.load(open("{{data.dl_model.json_file}}")))
+    model = tf.keras.models.model_from_json(
+        json_string, custom_objects=None
+    )
+
+    model.compile(optimizer='{{data.dl_model.optimizer}}',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['{{data.dl_model.metric}}'])
+
+    history = model.fit(x_training, y_training, epochs={{data.dl_model.epochs}})
+
+    test_loss, test_acc = model.evaluate(x_testing, y_testing, verbose=2)
+
+    return history, test_loss, test_acc
