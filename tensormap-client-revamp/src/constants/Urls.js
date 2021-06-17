cc94b261e710f6cb04a8a712884dfe8161ffb20e
Asela Dasanayaka <aselarbd@gmail.com>
2021-06-17 19:32:45 +0200

    [TensorMap-revamp] Finished the Home page
    Homepage of the TensorMap application is finished

--- tensormap-client-revamp/src/constants/Urls.js
+++ tensormap-client-revamp/src/constants/Urls.js
@@ -2,3 +2,4 @@ export const HOME_URL = "/home";
 export const DATA_UPLOAD_URL = "/data-upload";
 export const DATA_PROCESS_URL = "/data-process";
 export const DEEP_LEARN_URL = "/deep-learning";
+export const TENSOR_MAP_URL = "https://github.com/scorelab/TensorMap"
--- tensormap-client-revamp/src/containers/Home/Home.js
+++ tensormap-client-revamp/src/containers/Home/Home.js
@@ -1,9 +1,77 @@
 import React from 'react';
+import { Message, Segment, Grid, Header, Icon, Button } from 'semantic-ui-react';
+import { useHistory } from "react-router";
+import * as urls from '../../constants/Urls';
+import {TENSOR_MAP_URL} from "../../constants/Urls";
 
 const Home = () => {
+
+    const history = useHistory();
+
+    const handleButtonUpload = ()=> {
+        history.push(urls.DATA_UPLOAD_URL);
+    };
+
+    const handleButtonNewProject = ()=> {
+        history.push(urls.DEEP_LEARN_URL);
+    };
+
+    const handleButtonGoToGit = ()=> {
+        window.location.replace(TENSOR_MAP_URL);
+    }
+
     return (
         <div>
-            home 1
+            <Message size='huge'
+                icon='cogs'
+                header='TensorMap'
+                content='TensorMap is a web application that enables you to create deep learning models using a
+                graphical interface without having to know how to code. It is an open-source application.'
+            />
+
+            <Message size='big'
+                success
+                header='You can'
+                list={[
+                    'Upload Data for Preprocessing.',
+                    'Make Neural Network Models using Drag and Drop Feature.',
+                    'Visualise uploaded Data.',
+                    'Change Data Columns,Rows etc.',
+                    'Get Generated Code for Neural Network Model Made.'
+                ]}
+            />
+
+            <Segment placeholder>
+                <Grid columns={3} stackable textAlign='center'>
+
+
+                    <Grid.Row verticalAlign='middle'>
+                        <Grid.Column>
+                            <Header icon>
+                                <Icon name='cloud upload' />
+                                Upload Data
+                            </Header>
+                            <Button color='yellow' onClick={handleButtonUpload}>Upload</Button>
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <Header icon>
+                                <Icon name='file' />
+                                Create new Project
+                            </Header>
+                            <Button color='blue' onClick={handleButtonNewProject}>Create</Button>
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <Header icon>
+                                <Icon name='github' />
+                                Contribute at:
+                            </Header>
+                            <Button color='olive' onClick={handleButtonGoToGit}>Contribute</Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
         </div>
     );
 };
