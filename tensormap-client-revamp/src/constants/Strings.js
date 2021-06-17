89ac1f9aee6ca123e38725a47af099ba67b99786
Asela Dasanayaka <aselarbd@gmail.com>
2021-06-17 20:35:59 +0200

    [TensorMap-revamp] Updated home page
    Homepage updated with string variables

--- tensormap-client-revamp/src/constants/Strings.js
+++ tensormap-client-revamp/src/constants/Strings.js
@@ -2,3 +2,12 @@ export const HOME_SIDEBAR = "Home";
 export const DATA_UPLOAD_SIDEBAR = "Data Upload";
 export const DATA_PROCESS_SIDEBAR = "Data Processing";
 export const DEEP_LEARNING_SIDEBAR = "Deep leaning Modeling";
+export const HOME_MAIN_TITLE = "TensorMap";
+export const HOME_MAIN_CONTENT = "TensorMap is a web application that enables you to create deep learning models " +
+    "using a graphical interface without having to know how to code. It is an open-source application."
+export const HOME_ABILITY_TITLE = "You can";
+export const HOME_ABILITY_CONTENT_1 = "Upload Data for Preprocessing.";
+export const HOME_ABILITY_CONTENT_2 = "Make Neural Network Models using Drag and Drop Feature.";
+export const HOME_ABILITY_CONTENT_3 = "Visualise uploaded Data.";
+export const HOME_ABILITY_CONTENT_4 = "Change Data Columns,Rows etc.";
+export const HOME_ABILITY_CONTENT_5 = "Get Generated Code for Neural Network Model Made.";
