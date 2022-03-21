337edd7b8ff20c7cbfa2b0da16e41a4db3da1eb0
Oshan Ivantha <oshan.ivantha@gmail.com>
2022-03-21 07:22:12 +0530

    Move legacy components to a new directory

---
+++ legacy/tensormap-client/src/App.js
@@ -0,0 +1,33 @@
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import React from "react";
+import { Route, Router } from "react-router-dom";
+import styles from "./App.styles";
+import { history } from "./helpers";
+import { Dashboard } from "./pages/dashboard";
+import { Home } from "./pages/dashboard/scenes/home";
+
+class App extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Router history={history}>
+          <div>
+            <Route path="/" component={Dashboard} />
+            {/*<PrivateRoute exact path="/" component={Dashboard}/>*/}
+            {/*<Route path="/login" component={SignIn}/>*/}
+            {/*<Route path="/register" component={SignUp}/>*/}
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(App);
---
+++ legacy/tensormap-client/src/index.js
@@ -0,0 +1,22 @@
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import React              from 'react'
+import ReactDOM           from 'react-dom'
+import {Provider}         from 'react-redux'
+import App                from './App'
+import {store}            from './helpers'
+import './index.sass'
+import * as serviceWorker from './serviceWorker'
+import theme              from './theme'
+
+ReactDOM.render(
+    <Provider store={store}>
+      <MuiThemeProvider theme={theme}>
+        <App/>
+      </MuiThemeProvider>
+    </Provider>,
+    document.getElementById('root'))
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
