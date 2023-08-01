391f5a1f802feca30e2381eddb0e418669fe436d
Wenuka19 <89344987+Wenuka19@users.noreply.github.com>
2023-08-01 21:56:58 +0530

    Added new error messages to handle model run errors

--- tensormap-server/constants/errors.py
+++ tensormap-server/constants/errors.py
@@ -1,4 +1,6 @@
 err_msgs = {
     "A `Concatenate` layer requires inputs with matching shapes except for the concat axis.":"Incompatible inputs for multi-input layers.",
-    "Duplicate entry":"Model name already exists"
+    "Duplicate entry":"Model name already exists",
+    "incompatible with layer model":"MODEL RUNNING FAILED : Incompatible shapes for input or output layers",
+    "Incompatible shapes":"Error in Model values"
 }
