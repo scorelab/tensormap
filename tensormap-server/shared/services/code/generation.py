92bd505afb2c3c4f6d9871ec39e0f76538e57846
Wenuka19 <89344987+Wenuka19@users.noreply.github.com>
2023-08-01 21:56:19 +0530

    Redefined model run and code generation functions. Python file is not created at validation

--- tensormap-server/shared/services/code/generation.py
+++ tensormap-server/shared/services/code/generation.py
@@ -2,6 +2,7 @@ from endpoints.DataUpload.models import DataFile
 from jinja2 import Environment, FileSystemLoader
 from shared.constants import *
 from shared.services.config import get_configs
+from endpoints.DeepLearning.models import ModelBasic
 
 
 def code_generation(code_params):
@@ -37,6 +38,37 @@ def code_generation(code_params):
     generated_code_file.write(output + '\n')
     generated_code_file.close()
     return True
+
+def generate_code(model_name):
+    """
+    This service is used to generate code
+
+    :param code_params: parameters used to generate the code
+    :return: success of the operation
+    """
+    model_configs = ModelBasic.query.filter_by(model_name=model_name).first()
+
+    data = {
+        DATASET: {
+            FILE_NAME: helper_generate_file_location(file_id=getattr(model_configs, FILE_ID)),
+            FILE_TARGET: getattr(model_configs,FILE_TARGET),
+            MODEL_TRAINING_SPLIT: getattr(model_configs,MODEL_TRAINING_SPLIT),
+        },
+        LEARNING_MODEL: {
+            MODEL_JSON_FILE: helper_generate_json_model_file_location(model_name=model_name),
+            MODEL_OPTIMIZER: getattr(model_configs,MODEL_OPTIMIZER),
+            MODEL_METRIC: getattr(model_configs,MODEL_METRIC),
+            MODEL_EPOCHS: getattr(model_configs,MODEL_EPOCHS),
+        },
+    }
+
+    template_loader = FileSystemLoader(searchpath=TEMPLATE_ROOT)
+    template_env = Environment(loader=template_loader)
+    template = template_env.get_template(helper_map_correct_code_template(problem_type_id=model_configs.model_type))
+
+    output = template.render(data=data)
+    return output
+
 
 
 def helper_map_correct_code_template(problem_type_id):
